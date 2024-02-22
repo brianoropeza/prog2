0e6f0455dfd4cdfa9b8d35da0ad87fd8c47efc74
brian oropeza <brianoropeza123@gmail.com>
2024-02-22 14:35:45 -0500

    ejer15 terminado

---
+++ ejercicios/src/main/java/ejercicios/Ejercicio15.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicios;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Equipo
+ */
+public class Ejercicio15 {
+
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+        int valorA;
+        int valorB;
+        int valorC;
+
+        System.out.print("Introduce el valor A: ");
+        valorA = i.nextInt();
+        System.out.print("Introduce el valor B: ");
+        valorB = i.nextInt();
+
+        valorC = valorA;
+        valorA = valorB;
+        valorB = valorC;
+
+        System.out.println("nuevo valor A= " + valorA + " y nuevo valor B = " + valorB);
+    }
+}
