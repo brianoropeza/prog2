0f6bc7e8226722350cc9612c772c17f1ac99bbc3
brian oropeza <brianoropeza123@gmail.com>
2024-02-22 16:59:40 -0500

    ...

--- ejercicios/src/main/java/ejercicios1/Ejercicio11.java
+++ ejercicios/src/main/java/ejercicios1/Ejercicio11.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-package ejercicios;
+package ejercicios1;
 
 import java.util.Scanner;
 
